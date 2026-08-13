f3a16ec9c6131dd7c1122856f1d7606eddecbb2e
Kauan Rodrigues <rodrigueskauan2022@gmail.com>
2026-08-13 10:09:52 -0300

    feat: :sparkles: Controler
    Controller

--- pokedex/pokedex/src/main/java/com/pokedex/Controller/PokemonController.java
+++ pokedex/pokedex/src/main/java/com/pokedex/Controller/PokemonController.java
@@ -1,4 +1,22 @@
 package com.pokedex.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pokemon")
 public class PokemonController {
+
+    @GetMapping("/oi/{nome}")
+    public String HelloWorld(@PathVariable String nome){
+        return "Hello " + nome;
+    }
+
+    @GetMapping("/tchau/{nome}")
+    public String ByeWorld(@PathVariable String nome){
+        return "Bye " + nome;
+    }
+
 }
